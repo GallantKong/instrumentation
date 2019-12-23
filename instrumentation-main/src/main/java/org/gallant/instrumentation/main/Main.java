fb9887e749206321c91cb9690ad366b560e8d420
孔勇 <kongyong@dianwoda.com>
2019-12-23 15:34:19 +0800

    main2

--- instrumentation-main/src/main/java/org/gallant/instrumentation/main/Main.java
+++ instrumentation-main/src/main/java/org/gallant/instrumentation/main/Main.java
@@ -13,8 +13,22 @@ public class Main {
             System.out.println("Main.before.CountSheepTask.class.cl:"+ CountSheepTask.class.getClassLoader());
             new CountSheepTask().countSheep();
             Thread.sleep(1000 * 10);
+            // 重新转换或定义类之后，不用重新创建实例，变更后的方法已经生效，直接调用即可，也就是说下面的main2方法也会执行增强后的方法
             System.out.println("Main.after.CountSheepTask.class.cl:"+CountSheepTask.class.getClassLoader());
             new CountSheepTask().countSheep();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main2() {
+        try {
+            System.out.println("Main.before.CountSheepTask.class.cl:"+ CountSheepTask.class.getClassLoader());
+            CountSheepTask task = new CountSheepTask();
+            task.countSheep();
+            Thread.sleep(1000 * 10);
+            System.out.println("Main.after.CountSheepTask.class.cl:"+CountSheepTask.class.getClassLoader());
+            task.countSheep();
         } catch (Throwable e) {
             e.printStackTrace();
         }
